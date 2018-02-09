bf87268ceaae48ce649cfc63d5b3cfa9584c06dc
unknown <jdgrillone@gmail.com>
2018-02-09 12:01:59 -0700

    Added Title and Wrapper Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import characters from "./characters.json";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        characters.map(character => (
+      <Wrapper>
+        <Title>FFXIV Match Game</Title>
+        {characters.map(character => (
           <CharacterCard
             id={character.id}
             name={character.name}
             image={character.image}
           />
-        ))
-
+        ))}
+      </Wrapper>
     );
   }
 }
