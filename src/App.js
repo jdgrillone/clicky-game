d6c08b3e25e4c109a691982d1cb68c5f2fcb0a65
unknown <jdgrillone@gmail.com>
2018-02-09 11:49:27 -0700

    Initiated React app/Displaying all cards to DOM

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CharacterCard from "./components/CharacterCard";
+import characters from "./characters.json";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        characters.map(character => (
+          <CharacterCard
+            id={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))
+
     );
   }
 }
