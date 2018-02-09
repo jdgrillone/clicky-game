5c4a55dcc22bb76a197a7f2a73d2c792cd7e622f
unknown <jdgrillone@gmail.com>
2018-02-09 13:47:53 -0700

    Added shuffle function. Characters now render in random order.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,38 @@ import characters from "./characters.json";
 import './App.css';
 
 class App extends Component {
+
+  // Fisher-Yates shuffle function
+  shuffle = function (array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
   render() {
     return (
       <Wrapper>
         <Title>FFXIV Click Game</Title>
         <div className="card-wrapper">
-        {characters.map(character => (
-          <CharacterCard
-            id={character.id}
-            name={character.name}
-            image={character.image}
-          />
-        ))}
+          {this.shuffle(characters).map(character => (
+            <CharacterCard
+              id={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
         </div>
       </Wrapper>
     );
